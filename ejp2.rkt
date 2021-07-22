14d11e0525b055b2b3a32b61972f243f1a376fd9
Ramses López <ramseslopez99@ciencias.unam.mx>
2021-07-22 13:09:05 -0500

    2 completo

--- ejp2.rkt
+++ ejp2.rkt
@@ -20,21 +20,3 @@
               [else expr])]
     [(num n) expr]
     [(op f lst) (op f (map (lambda (x) (subst x sub-id value)) lst))]))
-
-
-(define (ids? e)
-  (match e
-    [(id i) #t]            
-    [(num n) #f]))
-
-(define (nids? e)
-  (match e
-    [(id i) #f]
-    [(num n) #t]))
-
-(define (idd e i v)
-  (match e
-    ['() '()]
-    [(cons (id l) xs) (cond
-                        [(equal? l i) v]
-                        [else (idd xs i v)])]))
