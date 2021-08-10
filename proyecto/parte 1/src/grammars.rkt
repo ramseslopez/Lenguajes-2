4ac4eb4086296211f1f5b02aa9e2315ad5ededa3
EricGiovanni <giovanny.m.t@ciencias.unam.mx>
2021-08-10 14:35:30 -0500

    Identando en archivos

--- proyecto/parte 1/src/grammars.rkt
+++ proyecto/parte 1/src/grammars.rkt
@@ -2,59 +2,59 @@
 
 ;; Definición del tipo Binding
 (define-type Binding
-  [binding (id symbol?) (value SCFWBAE?)])
+						 [binding (id symbol?) (value SCFWBAE?)])
 
 ;;Definición del tipo condition para la definición de cond.
 (define-type Condition
-  [condition (test-expr SCFWBAE?) (then-expr SCFWBAE?)]
-  [else-cond (else-expr SCFWBAE?)])
+						 [condition (test-expr SCFWBAE?) (then-expr SCFWBAE?)]
+						 [else-cond (else-expr SCFWBAE?)])
 
 ;; Definición del tipo SCFWBAEL
 (define-type SCFWBAE
-  [idS     (i symbol?)]
-  [numS    (n number?)]
-  [boolS   (b boolean?)]
-  [charS   (c char?)]
-  [stringS (s string?)]
-  [listS   (l (listof SCFWBAE?))]
-  [iFS     (test-expr SCFWBAE?) (then SCFWBAE?) (else SCFWBAE?)]
-  [iF0     (test-expr SCFWBAE?) (then SCFWBAE?) (else SCFWBAE?)]
-  [opS     (f procedure?) (args (listof SCFWBAE?))]
-  [condS   (cases (listof Condition?))]
-  [withS   (bindings (listof binding?)) (body SCFWBAE?)]
-  [withS*  (bindings (listof binding?)) (body SCFWBAE?)]
-  [funS    (params (listof symbol?)) (body SCFWBAE?)]
-  [appS    (fun SCFWBAE?) (args (listof SCFWBAE?))])
+						 [idS     (i symbol?)]
+						 [numS    (n number?)]
+						 [boolS   (b boolean?)]
+						 [charS   (c char?)]
+						 [stringS (s string?)]
+						 [listS   (l (listof SCFWBAE?))]
+						 [iFS     (test-expr SCFWBAE?) (then SCFWBAE?) (else SCFWBAE?)]
+						 [iF0     (test-expr SCFWBAE?) (then SCFWBAE?) (else SCFWBAE?)]
+						 [opS     (f procedure?) (args (listof SCFWBAE?))]
+						 [condS   (cases (listof Condition?))]
+						 [withS   (bindings (listof binding?)) (body SCFWBAE?)]
+						 [withS*  (bindings (listof binding?)) (body SCFWBAE?)]
+						 [funS    (params (listof symbol?)) (body SCFWBAE?)]
+						 [appS    (fun SCFWBAE?) (args (listof SCFWBAE?))])
 
 
 ;; Gramáticas de la práctica anterior. Esto lo utilizarás sólo si
 ;; quieres ganar los puntos extra. Puedes eliminarlo, en otro caso.
 ;; Definición del tipo CFWBAEL
 (define-type CFWBAE
-  [id     (i symbol?)]
-  [num    (n number?)]
-  [bool   (b boolean?)]
-  [chaR   (c char?)]
-  [strinG (s string?)]
-  [lisT   (l (listof CFWBAE?))]
-  [iF     (condicion CFWBAE?) (then CFWBAE?) (else CFWBAE?)]
-  [op     (f procedure?) (args (listof CFWBAE?))]
-  [fun    (params (listof symbol?)) (body CFWBAE?)]
-  [app    (fun CFWBAE?) (args (listof CFWBAE?))])
+						 [id     (i symbol?)]
+						 [num    (n number?)]
+						 [bool   (b boolean?)]
+						 [chaR   (c char?)]
+						 [strinG (s string?)]
+						 [lisT   (l (listof CFWBAE?))]
+						 [iF     (condicion CFWBAE?) (then CFWBAE?) (else CFWBAE?)]
+						 [op     (f procedure?) (args (listof CFWBAE?))]
+						 [fun    (params (listof symbol?)) (body CFWBAE?)]
+						 [app    (fun CFWBAE?) (args (listof CFWBAE?))])
 
 ;; Data-type que representa un caché de sustituciones
 (define-type DefrdSub
-  [mtSub]
-  [aSub  (name symbol?) (value CFWBAE-Value?) (ds DefrdSub?)])
+						 [mtSub]
+						 [aSub  (name symbol?) (value CFWBAE-Value?) (ds DefrdSub?)])
 
 ;;Data-type que representa la sintaxis abstracta de CFWBAE-Value
 (define-type CFWBAE-Value
-  [closure  (param (listof symbol?)) (body CFWBAE?) (env DefrdSub?)]
-  [numV     (n number?)]
-  [boolV    (b boolean?)]
-  [charV    (c char?)]
-  [stringV  (s string?)]
-  [listV    (l (listof CFWBAE-Value?))])
+						 [closure  (param (listof symbol?)) (body CFWBAE?) (env DefrdSub?)]
+						 [numV     (n number?)]
+						 [boolV    (b boolean?)]
+						 [charV    (c char?)]
+						 [stringV  (s string?)]
+						 [listV    (l (listof CFWBAE-Value?))])
 
 ;; Definición del procedimiento oR
 (define oR (lambda (x) (or x x)))
