fc6d93aea9ee626689b455de2948b9c3ec00877e
Ramses López <ramseslopez99@ciencias.unam.mx>
2021-09-01 02:33:50 -0500

    verifier en proceso

--- proyecto/parte 2/src/verifier.rkt
+++ proyecto/parte 2/src/verifier.rkt
@@ -15,18 +15,28 @@
     [(stringS s) (stringT)]
     [(listS lst) (map (lambda (x) (typeof x context)) lst)]
     [(opS f lst) (cond
-                   [(equal? f +) (cond
-                                   [(andmap numS? lst) (map (lambda (x) (typeof x context)) lst)]
-                                   [else (error 'typeof "tipos incorrectos")])]
-                   [(equal? f -) (cond
-                                   [(andmap numS? lst) (map (lambda (x) (typeof x context)) lst)]
-                                   [else (error 'typeof "tipos incorrectos")])]
-                   [(equal? f *) (cond
-                                   [(andmap numS? lst) (map (lambda (x) (typeof x context)) lst)]
-                                   [else (error 'typeof "tipos incorrectos")])]
-                   [(equal? f /) (cond
-                                   [(andmap numS? lst) (map (lambda (x) (typeof x context)) lst)]
-                                   [else (error 'typeof "tipos incorrectos")])])]))
+                   [(or (equal? f +) (equal? f -) (equal? f *) (equal? f /)
+                        (equal? f add1) (equal? f sub1) (equal? f modulo) (equal? f expt)
+                        (equal? f <) (equal? f <=) (equal? f =) (equal? f >=) (equal? f >)
+                        (equal? f zero?)) (cond
+                                            [(andmap numS? lst) (map (lambda (x) (typeof x context)) lst)]
+                                            [else (error 'typeof "tipos incorrectos")])]
+                   [(or (equal? f anD) (equal? f oR)
+                        (equal? f not)) (cond
+                                            [(andmap boolS? lst) (map (lambda (x) (typeof x context)) lst)]
+                                            [else (error 'typeof "tipos incorrectos")])]
+                   [(or (equal? f string-append)
+                        (equal? f string-length)) (cond
+                                                    [(andmap stringS? lst) (map (lambda (x) (typeof x context)) lst)]
+                                                    [else (error 'typeof "tipos incorrectos")])]
+                   [(or (equal? f car) (equal? f cdr)
+                       (equal? f length) (equal? f append)) (cond
+                                                              [(andmap listS? lst) (map (lambda (x) (typeof x context)) lst)]
+                                                              [else (error 'typeof "tipos incorrectos")])]
+                   [(equal? f cons) (cond
+                                      [(listS? (cdr lst)) (map (lambda (x) (typeof x context)) lst)]
+                                      [else (error 'typeof "tipos incorrectos")])]
+                   [else (map (lambda (x) (typeof x context)) lst)])]))
 
 ;; Busca el tipo correspondiente de un identificador
 ;; find-type :: SRCFWBAE Type-Context --> Type 
