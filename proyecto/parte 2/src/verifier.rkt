4c8d6cb7cb1bbf141a321e6ec8ef15b65c6de8b0
Ramses López <ramseslopez99@ciencias.unam.mx>
2021-09-01 21:15:51 -0500

    verifier casi listo

--- proyecto/parte 2/src/verifier.rkt
+++ proyecto/parte 2/src/verifier.rkt
@@ -14,75 +14,94 @@
     [(charS c) (charT)]
     [(stringS s) (stringT)]
     [(listS lst) (map (lambda (x) (typeof x context)) lst)]
-    [(opS f lst) (cond
-                   [(member f (list + - * / add1 sub1 modulo expt)) (cond
-                                            [(andmap (lambda (x) (numberT? (typeof x context))) lst) (numberT)]
-                                            [else (error (~a "typeof: Error in parameter: " (erroR lst "num") "\nExpected type: (numberT)\nGiven type: " (erroR lst "num")))])]
-                   [(member f (list < <= = >= >)) (cond
-                                                    [(andmap (lambda (x) (numberT? (typeof x context))) lst) (booleanT)]
-                                                    [else (error 'typeof "Los tipos deben ser number")])]
-                   [(equal? f zero?) (cond
-                                       [(numberT? (typeof (car lst) context)) (booleanT)]
-                                       [else (error 'typeof "El tipo debe ser number")])]
-                   [(equal? f not) (cond
-                                       [(booleanT? (typeof (car lst) context)) (booleanT)]
-                                       [else (error 'typeof "El tipo debe ser boolean")])]
-                   [(member f (list anD oR)) (cond
-                                            [(andmap (lambda (x) (booleanT? (typeof x context))) lst) (booleanT)]
-                                            [else (error 'typeof "Los tipos deben ser boolean")])]
-                   [(equal? f string-append) (cond
-                                                    [(andmap (lambda (x) (stringT? (typeof x context))) lst) (stringT)]
-                                                    [else (error 'typeof "Los tipos deben ser string")])]
-                   [(equal? f string-length) (cond
-                                       [(stringT? (typeof (car lst) context)) (numberT)]
-                                       [else (error 'typeof "El tipo debe ser string")])]
-                   [(equal? f car) (typeof (car (listS-l (car lst))) context)]
-                   [(equal? f length) (cond
-                                        [(listS? (car lst)) (numberT)]
-                                        [else (error 'typeof "El tipo debe ser una lista")])]
-                   [(member f (list cons append)) (flatten (map (lambda (x) (typeof x context)) lst))]
-                   [(equal? f cdr) (map (lambda (x) (typeof x context)) (cdr (listS-l (car lst))))]
-                   [else (booleanT)])]
-    [(iFS cnd thn els) (cond
-                         [(booleanT? (typeof cnd context)) (cond
-                                                             [(equal? (typeof thn context) (typeof els context)) (typeof thn context)]
-                                                             [else
-                                                              (error 'typeof "Type error\nconditionals must have same type in then-expr and else-expr")])]
-                         [else (error "if: Type error\nConditional's test-expr type must be a boolean\nGiven: " (typeof cnd context))])]
-    [(iF0 cnd thn els) (cond
-                         [(numberT? (typeof cnd context)) (cond
-                                                             [(equal? (typeof thn context) (typeof els context)) (typeof thn context)]
-                                                             [else
-                                                              (error 'typeof "Type error\nconditionals must have same type in then-expr and else-expr")])]
-                         [else (error "if: Type error\nConditional's test-expr type must be a boolean\nGiven: " (typeof cnd context))])]
+    [(opS f lst) (type-op f lst context)]
+    [(iFS cnd thn els) (type-if expr context)]
+    [(iF0 cnd thn els) (type-if expr context)]
     [(condS cnds) (type-cond cnds context)]))
 
 ;; Busca el tipo correspondiente de un identificador
-;; find-type :: SRCFWBAE Type-Context --> Type 
+;; find-type :: SRCFWBAE Type-Context --> Type
 (define (find-type expr context)
   (match context
-    [(phi) (error 'find-type "Identificador libre")]
+    [(phi) (error 'find-type "Free variable: " expr)]
     [(gamma id type rest) (cond
                             [(equal? id expr) type]
                             [else (find-type expr rest)])]))
 
-(define (get-cond cnd n)
-  (match cnd
-    [(condition a b) (if (= n 1)
-                         a
-                         b)]
-    [(else-cond c) (if (= n 1)
-                       c
-                       #f)]))
+;; Obtiene el tipo general de una operación
+;; type-op :: procedure (listof SRCFWBAE) Type-Context --> Type
+(define (type-op f lst context)
+  (cond
+    [(member f (list + - * / add1 sub1 modulo expt))
+     (cond
+       [(andmap (lambda (x) (numberT? (typeof x context))) lst) (numberT)]
+       [else (error
+              (string-append "typeof: Error in parameter: " (~v (erroR lst "num" context))
+                             "\nExpected type: (numberT)\nGiven type: " (~v (typeof (erroR lst "num" context) context))))])]
+    [(member f (list < <= = >= >))
+     (cond
+       [(andmap (lambda (x) (numberT? (typeof x context))) lst) (booleanT)]
+       [else (error
+              (string-append "typeof: Error in parameter:" (~v (erroR lst "num" context))
+                             "\nExpected type: (numberT)\nGiven type: " (~v (typeof (erroR lst "num" context) context))))])]
+    [(equal? f zero?)
+     (cond
+       [(numberT? (typeof (car lst) context)) (booleanT)]
+       [else (error
+              (string-append "typeof: Error in parameter:" (~v (erroR lst "num" context))
+                             "\nExpected type: (numberT)\nGiven type: " (~v (typeof (erroR lst "num" context) context))))])]
+    [(equal? f not)
+     (cond
+       [(booleanT? (typeof (car lst) context)) (booleanT)]
+       [else (error
+              (string-append "typeof: Error in parameter:" (~v (erroR lst "bool" context))
+                             "\nExpected type: (booleanT)\nGiven type: " (~v (typeof (erroR lst "bool" context) context))))])]
+    [(member f (list anD oR))
+     (cond
+       [(andmap (lambda (x) (booleanT? (typeof x context))) lst) (booleanT)]
+       [else (error
+              (string-append "typeof: Error in parameter:" (~v (erroR lst "bool" context))
+                             "\nExpected type: (booleanT)\nGiven type: " (~v (typeof (erroR lst "bool" context) context))))])]
+    [(equal? f string-append)
+     (cond
+       [(andmap (lambda (x) (stringT? (typeof x context))) lst) (stringT)]
+       [else (error
+              (string-append "typeof: Error in parameter:" (~v (erroR lst "str" context))
+                             "\nExpected type: (stringT)\nGiven type: " (~v (typeof (erroR lst "str" context) context))))])]
+    [(equal? f string-length)
+     (cond
+       [(stringT? (typeof (car lst) context)) (numberT)]
+       [else (error
+              (string-append "typeof: Error in parameter:" (~v (erroR lst "str" context))
+                             "\nExpected type: (stringT)\nGiven type: " (~v (typeof (erroR lst "str" context) context))))])]
+    [(equal? f car) (typeof (car (listS-l (car lst))) context)]
+    [(equal? f length) (cond
+                         [(listS? (car lst)) (numberT)]
+                         [else (error 'typeof "El tipo debe ser una lista")])]
+    [(member f (list cons append)) (flatten (map (lambda (x) (typeof x context)) lst))]
+    [(equal? f cdr) (map (lambda (x) (typeof x context)) (cdr (listS-l (car lst))))]
+    [else (booleanT)]))
 
-#|(define (type-cond cnd context prev-cnd)
-  (map (lambda (x) (match cnd
-                     [(condition a b) (if (booleanT? (typeof a context))
-                                          (if (or (equal? prev-cnd (typeof b context)) (equal? prev-cnd "N"))
-                                              (type-cond )
-                                              ())
-                                          ())])) ))|#
+;; Obtiene el tipo general de una expresión if
+;; type-if :: SRCFWBAE Type-Context --> Type
+(define (type-if expr context)
+  (match expr
+    [(iFS cnd thn els)
+     (cond
+       [(booleanT? (typeof cnd context))
+        (cond
+          [(equal? (typeof thn context) (typeof els context)) (typeof thn context)]
+          [else (error 'typeof "Type error\nConditionals must have same type in then-expr and else-expr")])]
+       [else (error "if: Type error\nConditional's test-expr type must be a boolean\nGiven: " (typeof cnd context))])]
+    [(iF0 cnd thn els)
+     (cond
+       [(numberT? (typeof cnd context))
+        (cond
+          [(equal? (typeof thn context) (typeof els context)) (typeof thn context)]
+          [else (error 'typeof "Type error\nConditionals must have same type in then-expr and else-expr")])]
+       [else (error "if0: Type error\nConditional's test-expr type must be a number\nGiven: " (typeof cnd context))])]))
 
+#|
 (define (type-cond cnd context)
   (let ([prev-b '()])
     (if (andmap (lambda (x) (match x
@@ -91,36 +110,51 @@
                                                      (if (or (equal? prev-b t)
                                                              (equal? prev-b '()))
                                                          #t
-                                                         (error "type-cond: then tiene tipos distintos: " b))
+                                                         (error 'typeof "Type error\nConditionals must have same type in then-expr's"))
                                                      (set! prev-b t))
-                                                   (error "type-cond: condition no es boolean: " a))]
+                                                   (error "cond: Type error\nConditional's test-expr type must be a number\nGiven: " (typeof a context)))]
                               [(else-cond c) (if (equal? prev-b (typeof c context))
                                                  #t
-                                                 (error "type-cond: else tiene distinto tipo " c))])) cnd)
+                                                 (error 'typeof "Type error\nConditionals must have same type in then-expr and else-expr"))])) cnd)
         prev-b
-        (error 'type-cond "error de tipo"))))
+        (error 'type-of "Type error\nConditionals"))))|#
+
+;; Obtiene el tipo general de una expresión cond
+;; type-cond :: SRCFWBAE Type-Context --> Type
+(define (type-cond cnds context)
+  (let ([prev-b '()])
+    (cond
+      [(andmap (lambda (x)
+                 (match x
+                   [(condition a b) (cond
+                                      [(booleanT? (typeof a context))
+                                       (let ([type-b (typeof b context)])
+                                         (cond
+                                           [(or (equal? prev-b type-b) (equal? prev-b '())) #t]
+                                           [else (error 'typeof "Type error\nConditionals must have same type in then-expr's")])
+                                         (set! prev-b type-b))]
+                                      [else (error "cond: Type error\nConditional's test-expr type must be a number\nGiven: " (typeof a context))])]
+                   [(else-cond c) (cond
+                                    [(equal? prev-b (typeof c context)) #t]
+                                    [else (error 'typeof "Type error\nConditionals must have same type in then-expr and else-expr")])])) cnds) prev-b]
+      [else (error 'type-of "Type error\nConditionals")])))
 
 
-(define (erroR lst type)
+(define (erroR lst type context)
   (match type
     ["num" (cond
                [(empty? lst) empty]
-               [(not (numS? (car lst))) (car lst)]
-               [else (erroR (cdr lst) "num")])]
+               [(not (numberT? (typeof (car lst) context))) (car lst)]
+               [else (erroR (cdr lst) "num" context)])]
     ["bool" (cond
                [(empty? lst) empty]
-               [(not (boolS? (car lst))) (car lst)]
-               [else (erroR (cdr lst) "bool")])]
+               [(not (booleanT? (typeof (car lst) context))) (car lst)]
+               [else (erroR (cdr lst) "bool" context)])]
     ["char" (cond
                [(empty? lst) empty]
-               [(not (charS? (car lst))) (car lst)]
-               [else (erroR (cdr lst) "char")])]
+               [(not (charT? (typeof (car lst) context))) (car lst)]
+               [else (erroR (cdr lst) "char" context)])]
     ["str" (cond
                [(empty? lst) empty]
-               [(not (stringS? (car lst))) (car lst)]
-               [else (erroR (cdr lst) "str")])]))
-
-#|cond
-    [(empty? lst) empty]
-    [(not (numS? (car lst))) (car lst)]
-    [else (erroR (cdr lst))]|#
+               [(not (stringT? (typeof (car lst) context))) (car lst)]
+               [else (erroR (cdr lst) "str" context)])]))
